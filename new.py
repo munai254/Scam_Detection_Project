392ebd52e8f52dcfcdb5d2f1f02a4f24d95db306
Isaack Karanjah <isaackaris.52@gmail.com>
2025-01-22 12:45:38 +0300

    links

---
+++ new.py
@@ -0,0 +1,11 @@
+#link 1 github url 
+#https://github.com/shakiliitju/Credit-Card-Fraud-Detection-Using-Machine-Learning
+
+#link 2 github url
+#https://github.com/ThecoderPinar/Credit-Card-Fraud-Detection-Project
+
+#geekforgeek
+#https://www.geeksforgeeks.org/ml-credit-card-fraud-detection/
+
+#procedure
+#https://medium.com/@rashmilis1/credit-card-fraud-detection-a-data-science-project-3f6510d36e54
